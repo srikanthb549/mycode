1d73c730d9aa37ecdabfaeda22ed9c4533a5297c
srikanthb549 <sbakkolla@gmail.com>
2024-11-01 16:05:55 +0530

    commit5

---
+++ 02-2_EC2_route/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "instance" {
+  count = length(var.ec2-instances)
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0781e11c5f22234f7"]
+  tags = {
+    Name = var.ec2-instances[count.index]
+  }
+}
+
+variable "ec2-instances" {
+  default = [
+    "qa1",
+    "qa2"]
+}
+
+
+resource "aws_route53_record" "route53" {
+  count = length(var.ec2-instances)
+  name    = var.ec2-instances[count.index]
+  type    = "A"
+  zone_id = "Z08641142SH8HBQE1FAJT"
+  ttl     = "30"
+  records = [aws_instance.instance[count.index].private_ip]
+}
+
