71d72fbc805258c483e0cd14ac14fd900d4f3a46
srikanthb549 <sbakkolla@gmail.com>
2024-11-01 14:16:53 +0530

    commit5

---
+++ 01-modules/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "instance" {
+  count = length(var.ec2-instances)
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0781e11c5f22234f7"]
+  tags = {
+    name = "var.ec2-instances[count.index]"
+  }
+}
+
+variable "ec2-instances" {
+  default = [ "prod1" ]
+}
+
+
+resource "aws_route53_record" "route53" {
+  count = length(var.ec2-instances)
+  name    = "var.ec2-instances[count.index]"
+  type    = "A"
+  zone_id = "Z08641142SH8HBQE1FAJT"
+  ttl     = "30"
+  records = [aws_instance.instance[count.var.ec2-instances].private_ip]
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mycode
+#01-modules 
+    #Trying to Create a Single Ec2 instance along with route53 record.
+
