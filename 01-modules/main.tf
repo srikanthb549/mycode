c6df02265fc7b9662dd57e4a67a647002f567b6c
srikanthb549 <sbakkolla@gmail.com>
2024-11-01 14:53:19 +0530

    commit5

--- 01-modules/main.tf
+++ 01-modules/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "instance" {
   instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-0781e11c5f22234f7"]
   tags = {
-    name = "var.ec2-instances[count.index]"
+    name = var.ec2-instances[count.index]
   }
 }
 
@@ -15,7 +15,7 @@ variable "ec2-instances" {
 
 resource "aws_route53_record" "route53" {
   count = length(var.ec2-instances)
-  name    = "var.ec2-instances[count.index]"
+  name    = var.ec2-instances[count.index]
   type    = "A"
   zone_id = "Z08641142SH8HBQE1FAJT"
   ttl     = "30"
